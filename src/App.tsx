5b4bf92830339ffa14aef63d921cb3aabfcd3f11
Samuel Deotti <samuel_deotti@hotmail.com>
2023-09-07 21:24:18 -0300

    movie card, genres card

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
 import Layout from './Pages/Layout/Layout';
+import Movie from './Pages/Movie/Movie';
+import SearchedMovies from './Pages/SearchedMovies/SearchedMovies';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
         <Route index element={ <Home /> } />
+        <Route path="movie/:id" element={ <Movie /> } />
+        <Route path="/genre/:id/:name" element={ <SearchedMovies /> } />
       </Route>
       {/* <Route path='/*' element={ <NotFound /> }></Route> */}
     </Routes>
--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header.tsx/Header';
+import NavMenu from '../../components/NavMenu/NavMenu';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main>
+      <main style={ { display: 'flex' } }>
+        <NavMenu />
         <Outlet />
       </main>
     </>
