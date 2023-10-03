9800b6bee17b9ca69bb0624522c0aaa44e27cfec
Samuel Deotti <samuel_deotti@hotmail.com>
2023-10-03 16:17:10 -0300

    starting css

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n.ts';
 import App from './App.tsx';
-import './index.css';
+import GlobalStyles from './styles/global-styles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nextProvider i18n={ i18n }>
       <BrowserRouter>
+        <GlobalStyles />
         <App />
       </BrowserRouter>
     </I18nextProvider>
